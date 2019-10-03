66183b1830314369028fae0ad4672ecd9d2e0223
Daniel Kukula <daniel.kuku@gmail.com>
2019-10-03 23:30:34 +0100

    jednak tailwind - da sie global style

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,7 +30,17 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'postcss-loader', options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer')
+            ]
+          }}
+        ]
       }
     ]
   },
