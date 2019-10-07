ab5dd3b4bb449c1a1a47fe18fc917d2fcec25c13
Daniel Kukula <daniel.kuku@gmail.com>
2019-10-07 21:58:01 +0100

    remove elixir.lorem

--- lib/workpermit_web/controllers/page_controller.ex
+++ lib/workpermit_web/controllers/page_controller.ex
@@ -1,11 +1,8 @@
 defmodule WorkpermitWeb.PageController do
   use WorkpermitWeb, :controller
   plug :put_layout, "landing.html"
-  alias Elixilorem
   def index(conn, _params) do
     conn
-    |> assign(:title, Elixilorem.words(3))
-    |> assign(:body, Elixilorem.paragraph())
     |> assign(:time, Timex.today())
     |> render("index.html")
   end
