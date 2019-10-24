24ac50b74da42deb0e263cebd1af21454d9d1967
Daniel Kukula <daniel.kuku@gmail.com>
2019-10-24 22:52:42 +0100

    view helpers

--- lib/workpermit_web/views/permit_view.ex
+++ lib/workpermit_web/views/permit_view.ex
@@ -1,6 +1,7 @@
 defmodule WorkpermitWeb.PermitView do
   use WorkpermitWeb, :view
   alias WorkpermitWeb.Markdown
+  alias Workpermit.IsoSymbols
   require EEx
 
   def markdown(body) do
@@ -8,6 +9,16 @@ defmodule WorkpermitWeb.PermitView do
     |> Markdown.render
     |> raw
   end
+  def iso_image(img), do: img |> IsoSymbols.symbol_to_image_path
+  def iso_meaning(img), do: img |> IsoSymbols.symbol_to_sign_meaning
+
+  def date_time(%{} = date), do: date |> Timex.format!("%Y/%m/%d %H:%M", :strftime)
+  def date_time(_), do: gettext("No data")
+  def time(%{} = date), do: date |> Timex.format!("%H:%M", :strftime)
+  def time(_), do: gettext("No data")
+  def date(%{} = date), do: date |> Timex.format!("%Y/%m/%d", :strftime)
+  def date(_), do: gettext("No data")
+  def full_name(%{first_name: f, last_name: l}), do: "#{f} #{l}"
 
   def permit_datetime_select(form, field, opts \\ []) do
     builder = fn b ->
@@ -15,15 +26,20 @@ defmodule WorkpermitWeb.PermitView do
 
       class = "form-input mt-1"
 
-      [~E"<div>Year: ",
+      [~E"<div>",
+        gettext("Year"),
         b.(:year, [options: year_select(now), value: now.year, class: class]),
-        ~E" </div><div> Month: ", 
+        ~E" </div><div>",
+        gettext("Month"),
         b.(:month, [options: month_select(now), value: now.month, class: class]),
-        ~E" </div><div> Day: ", 
+        ~E" </div><div>",
+        gettext("Day"),
         b.(:day, [options: day_select(now), value: now.day, class: class]),
-        ~E" </div><div> Hour: ", 
+        ~E" </div><div>",
+        gettext("Hour"),
         b.(:hour, [options: hour_select(now), class: class]),
-        ~E" </div><div> Minute: ", 
+        ~E" </div><div>",
+        gettext("Minutes"),
         b.(:minute, [options: minute_select(), class: class]),
         ~E"</div>"
       ]
@@ -31,19 +47,15 @@ defmodule WorkpermitWeb.PermitView do
     datetime_select(form, field, [builder: builder] ++ opts)
   end
 
-  defp year_select(now) do
-    year = [now.year]
-    year = if now.month == 1 and now.day == 1, do: [now.year, now.year - 1], else: year
-    year = if now.month == 12 and now.day > Timex.shift(now, days: +4).day, do: [now.year, now.year + 1], else: year
-    year
-  end
+  defp year_select(%{year: y, month: 1, day: 1}), do: [y, y - 1]
+  defp year_select(%{year: y, month: 12, day: d}) when d in 28..31, do: [y, y + 1]
+  defp year_select(%{year: y}), do: [y]
 
-  defp month_select(now) do
-    month = [now.month]
-    month = if now.day == 1, do: [Timex.shift(now, months: -1).month, now.month], else: month
-    month = if now.day > Timex.shift(now, days: 4).day, do: [now.month, Timex.shift(now, months: 1).month], else: month
-    month
-  end
+  defp month_select(%{month: 1, day: 1}), do: [1, 12] 
+  defp month_select(%{month: m, day: 1}), do: [m, m - 1] 
+  defp month_select(%{month: 12, day: d}) when d in 27..31, do: [12, 1] 
+  defp month_select(%{month: m, day: d}) when d in 27..31, do: [m, m + 1] 
+  defp month_select(%{month: m}), do: [m] 
 
   defp day_select(now) do
     [
