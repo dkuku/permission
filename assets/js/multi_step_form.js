d11681a55c76bda350fdaa7d7b40bc906100f148
Daniel Kukula <daniel.kuku@gmail.com>
2019-10-26 19:36:00 +0100

    style permit form, scope css

--- assets/js/multi_step_form.js
+++ assets/js/multi_step_form.js
@@ -47,11 +47,10 @@ function nextPrev(n) {
   // This function will figure out which tab to display
 function validateForm() {
   // This function deals with validation of the form fields
-  return true;
-  //var x, y, i, valid = true;
-  //x = document.getElementsByClassName("tab");
-  //y = x[currentTab].getElementsByTagName("input");
-  //// A loop that checks every input field in the current tab:
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
   //for (i = 0; i < y.length; i++) {
   //  // If a field is empty...
   //  if (y[i].value == "") {
@@ -61,14 +60,14 @@ function validateForm() {
   //    valid = false;
   //  }
   //}
-  //// If the valid status is true, mark the step as finished and valid:
-  //if (valid) {
-  //  document.getElementsByClassName("step")[currentTab].className += " finish";
-  //} else {
-  //  document.getElementsByClassName("step")[currentTab].className.replace("finish", "");
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  } else {
+    document.getElementsByClassName("step")[currentTab].className.replace("finish", "");
 
-  //}
-  //return valid; // return the valid status
+  }
+  return valid; // return the valid status
 }
 
 function fixStepIndicator(n) {
