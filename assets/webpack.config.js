9bec504b7b75144dd8c5b28d15a417738322edef
Daniel Kukula <daniel.kuku@gmail.com>
2020-01-13 21:35:06 +0000

    update templates paths

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -5,6 +5,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PurgecssPlugin = require('purgecss-webpack-plugin');
+const globAll = require('glob-all');
 
 class TailwindExtractor {
   static extract(content) {
@@ -18,11 +19,16 @@ module.exports = (env, options) => ({
       new TerserPlugin(),
       new OptimizeCSSAssetsPlugin({}),
       new PurgecssPlugin({
-        paths: glob.sync('../lib/workpermit_web/templates/**/*.html.eex'),
+        paths: globAll.sync([
+            '../lib/web/templates/**/*.html.eex',
+            '../lib/web/views/**/*.ex',
+            '../lib/web/live/**/*.ex',
+            '../assets/js/**/*.js',
+          ]),
         extractors: [
           {
             extractor: TailwindExtractor,
-            extensions: ['html', 'js', 'eex'],
+            extensions: ['html', 'js', 'eex', 'ex'],
           },
         ],
         whitelist: ['.markdown', '.awesomplete']
